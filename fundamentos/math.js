f1be68e24512812096c8f618c1285ee3dc2d29a4
Larissa Oliveira <larioli.me@gmail.com>
2020-01-05 23:56:13 -0300

    Adicionando exemplo da utilização do objeto math

---
+++ fundamentos/math.js
@@ -0,0 +1,7 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+
+// A função Math.pow() retorna a base elevada ao expoente power, ou seja, baseexpoente.
+
+console.log(area)
+console.log(typeof Math)
