95fea419cface1c5b38f2be5647463f389b729a9
Larissa Oliveira <larioli.me@gmail.com>
2020-01-05 23:32:46 -0300

    Adicionando exemplo sobre tipagem fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,9 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.5010
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// No caso acima podemos observar que a mesma variável pode ter múltiplos valores
