51cfa1d4141731cd91e3ee0b86fa1dd200da55ad
Larissa Oliveira <larioli.me@gmail.com>
2020-01-15 10:57:20 -0300

    Adicionando exemplo de objeto

---
+++ fundamentos/objeto.js
@@ -0,0 +1,24 @@
+// Declarando um array
+//const prod1 = []
+
+// Declarando um objeto
+// Objeto = coleção de chaves e valores
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    obj: {
+        blabla: 1,
+        obj: {
+            blabla: 2
+        }
+    }
+}
+
+console.log(prod2)
