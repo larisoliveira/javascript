57a35b7eb2a54541a463cf0bf1447629ea708e8b
Larissa Oliveira <larioli.me@gmail.com>
2020-02-05 08:54:16 -0300

    Adicionando segundo exemplo de objeto

---
+++ fundamentos/objeto2.js
@@ -0,0 +1,11 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+// Abaixo é apenas uma sintaxe diferente para a declaração de função
+class Produto {} // ES 2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto)
