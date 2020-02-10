e6c7bcd3bc54bdd3bd0a9ef5164ee45d9e9eeb58
Larissa Oliveira <larioli.me@gmail.com>
2020-02-10 10:23:47 -0300

    Adicionando quarto exemplo usando destructuring com função

---
+++ fundamentos/destructuring4.js
@@ -0,0 +1,8 @@
+// Caso o mínimo seja maior que o máximo, então iremos inverter os valores
+function rand([min = 0, max = 1000]) {
+  if (min > max) [min, max] = [max, min]
+  const valor = Math.random() * (max - min) + min
+  return Math.floor(valor)
+}
+
+console.log(rand([50, 40]))
