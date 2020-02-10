9e33bc40c615de01993dd64aba7df465b3e71f61
Larissa Oliveira <larioli.me@gmail.com>
2020-02-10 09:34:59 -0300

    Adicionando o segundo exemplo usando destructuring

---
+++ fundamentos/destructuring2.js
@@ -0,0 +1,7 @@
+
+const [a] = [10]
+console.log(a)
+
+// Abaixo temos vários elementos ao qual extraímos e adicionamos os valores nas variáveis 
+const [n1, , n3, , n5, n6 = 0] = [10, 7, 9, 8]
+console.log(n1, n3, n5, n6)
