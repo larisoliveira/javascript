936030381a2becd8d8c145fe192549ee5909f0a1
Larissa Oliveira <larioli.me@gmail.com>
2020-02-11 08:39:44 -0300

    Adicionando exemplos de operadores lógicos

---
+++ fundamentos/relacionais.js
@@ -0,0 +1,33 @@
+// Comparando os valores
+console.log('01)', '1' == 1)
+
+// Comparando os valores e o tipo
+console.log('02)', '1' === 1)
+
+// Comparando se os valores são direntes
+console.log('03)', '3' != 3)
+
+// Comparando se os valores e o tipo são diferentes
+console.log('04)', '3' !== 3)
+
+// Comparando se 3 é menor que 2
+console.log('05)', 3 < 2)
+
+// Comparando se 3 é maior que 2
+console.log('06)', 3 > 2)
+
+// Comparando se 3 é menor OU igual a 2
+console.log('07)', 3 <= 2)
+
+// Comparando se 3 é maior OU igual a 2
+console.log('08)', 3 >= 2)
+
+// Comparando a referência de memória
+const d1 = new Date(0)
+const d2 = new Date(0)
+console.log('09)', d1 === d2)
+console.log('10)', d1 == d2)
+
+console.log('11)', undefined == null)
+console.log('12)', undefined == null)
+
