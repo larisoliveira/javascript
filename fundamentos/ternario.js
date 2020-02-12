e144a5a5cafbe375e3e575fb87508a19a4b09878
Larissa Oliveira <larioli.me@gmail.com>
2020-02-12 09:17:16 -0300

    Adicionando exemplo de ternário

---
+++ fundamentos/ternario.js
@@ -0,0 +1,5 @@
+// Abaixo utilizamos o ternário para nos retornar um valor. Basicamente, a estrutura abaixo é quase que um IF e ELSE
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(5))
+console.log(resultado(7.1))
